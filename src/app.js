996565124d8ca93c3a964f78dac98fef3d890c91
mlopezmo <85364227+mlopezmo@users.noreply.github.com>
2021-06-29 19:53:14 +0200

    adding forecast

--- src/app.js
+++ src/app.js
@@ -37,7 +37,38 @@ function displayForecast (response){
   let forecast = response.data.daily;
   let forecast = document.querySelector ("#forecast");
   let forecastHTML = `<div class="row">`;
-}
+  forecast.forEach (function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+        }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+          forecastDay.temp.max
+        )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+          forecastDay.temp.min
+        )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  
+  }
+
 // function for temperature
 
 function displayTemperature(response) {
